3cf73b5d1fa820566e4c325021ebbbb44d229722
Jang2723 <jangsb46@gmail.com>
2023-11-28 17:10:17 +0900

    ADD W1 B11382/B2588

---
+++ src/W1/B11382.java
@@ -0,0 +1,15 @@
+package W1;
+import java.util.Scanner;
+
+public class B11382 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        Long a = scanner.nextLong();
+        Long b = scanner.nextLong();
+        Long c = scanner.nextLong();
+
+        System.out.println(a+b+c);
+    }
+
+}
---
+++ src/W1/B2588.java
@@ -0,0 +1,19 @@
+package W1;
+
+import java.util.Scanner;
+
+public class B2588{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println(A*(B%10));
+        System.out.println(A*(B%100/10));
+        System.out.println(A*(B/100));
+        System.out.println(A*B);
+    }
+
+}
