6f1c2a57df0e8403349ed1149a2ea932331ee439
Jang2723 <jangsb46@gmail.com>
2023-12-04 17:01:29 +0900

    W2 B2480 & B25304

---
+++ src/W2/B2480.java
@@ -0,0 +1,50 @@
+package W2;
+
+import java.util.Scanner;
+
+/* 주사위 3개를 던져
+같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다.
+같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다.
+모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.
+3개 주사위의 나온 눈이 주어질 때, 상금을 계산하는 프로그램을 작성 하시오.
+*/
+public class B2480 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int reward = 0;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // a,b가 같을 떄
+        if ( a == b){
+            if(a == c){ //abc 10,000원+(같은 눈)×1,000원
+                reward = 10000 + a * 1000;
+            }
+            else { //ab  1,000원+(같은 눈)×100
+                reward = 1000 + a * 100;
+            }
+        }
+        else{ // a,b가 다를때
+            if(b == c) {//bc  1,000원+(같은 눈)×100
+                reward = 1000 + b * 100;
+            } else if (a == c) { // ac  1,000원+(같은 눈)×100
+                reward = 1000 + a * 100;
+            } else{ //(그 중 가장 큰 눈)×100원
+                // a,b,c 모두 다름 제일 큰 수 구하기
+                int max = 0;
+                if(a>b){
+                    if(a>c) max = a;
+                    else max = c;
+                }
+                else{
+                    if(b>c) max = b;
+                    else max = c;
+                }
+                reward = max * 100;
+            }
+        }
+        System.out.println(reward);
+
+    }
+}
---
+++ src/W2/B25304.java
@@ -0,0 +1,34 @@
+package W2;
+
+import java.util.Scanner;
+
+/*
+구매한 각 물건의 가격과 개수
+구매한 물건들의 총 금액
+을 보고, 구매한 물건의 가격과 개수로 계산한 총 금액이
+영수증에 적힌 총 금액과 일치하는지 검사
+*/
+public class B25304 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 영수증에 적힌 총 금액 x
+        int x = sc.nextInt();
+        // 구매한 물건의 종류 n
+        int n = sc.nextInt();
+        // 내가 계산한 총 금액
+        int sum = 0;
+        // n개의 줄에는 물건의 가격 a와 개수 b가 공백을 두고 주어짐
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            sum += a * b;
+        }
+        if (x == sum){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+
+        }
+    }
+}
