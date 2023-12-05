696e8950aef2070f5fbb51689e190ee37043dbe0
Jang2723 <jangsb46@gmail.com>
2023-12-05 16:16:43 +0900

    B10807 & B25314

---
+++ src/W2/B10807.java
@@ -0,0 +1,24 @@
+package W2;
+
+import java.util.Scanner;
+
+// 총 n개의 정수가 주여졌을 때, 정수 v가 몇개인지 구하는 프로그램을 작성
+// 첫쨰 줄 입력될 정수의 총 수, 둘쨰 줄 정수들, 셋쨰 줄 찾아야 할 정수 v
+public class B10807 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i <n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int v = sc.nextInt(); // 찾아야 할 정수
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == v){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
---
+++ src/W2/B25314.java
@@ -0,0 +1,18 @@
+package W2;
+// 4바이트는 long int, 12바이트는 long long long int라고 생각할 떄
+// n바이트는 어떤 출력형태를 갖는가?
+
+import java.util.Scanner;
+
+public class B25314 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // long 몇번 출력해야 하는지 count 하기
+        int count = n / 4;
+        for(int i=0; i< count; i++){
+            System.out.print("long ");
+        }
+        System.out.println("int");
+    }
+}
