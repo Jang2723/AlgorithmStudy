51fa2cf5da6a6e6a17f6c65a8d62a47710438c9b
Jang2723 <jangsb46@gmail.com>
2023-12-05 16:59:26 +0900

    Solution181883 & 181884 & 181914

---
+++ src/W2/Solution181884.java
@@ -0,0 +1,15 @@
+package W2;
+//https://school.programmers.co.kr/learn/courses/30/lessons/181884
+// n보다 커질 떄까지 더하기
+public class Solution181884 {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if(answer > n) return answer;
+            else{
+                answer += numbers[i];
+            }
+        }
+        return answer;
+    }
+}
