1d0c616222adf4060e215fa25abaa512ca80cea6
Jang2723 <jangsb46@gmail.com>
2023-12-12 17:59:47 +0900

    12/12

---
+++ src/W3/extraStudy/B19532.java
@@ -0,0 +1,28 @@
+package W3.extraStudy;
+// https://www.acmicpc.net/problem/19532
+// 수학은 비대면강의입니다.
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B19532 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int a = Integer.parseInt(str[0]);
+        int b = Integer.parseInt(str[1]);
+        int c = Integer.parseInt(str[2]);
+        int d = Integer.parseInt(str[3]);
+        int e = Integer.parseInt(str[4]);
+        int f = Integer.parseInt(str[5]);
+
+        for (int i = -999; i < 1000; i++) {
+            for (int j = -999; j < 1000; j++) {
+                if (a * i + b * j == c && d * i + e * j == f){
+                    System.out.println(i + " " + j);
+                }
+            }
+        }
+    }
+}
--- src/W3/extraStudy/README.md
+++ src/W3/extraStudy/README.md
@@ -2,12 +2,10 @@
 
 12/09
 - [B1152](B1152.java) - 문자열
+- [B19532](B19532.java) - 브루트포스 알고리즘
 
 ---- 풀 예정 ----
 ## 난이도 중
-
-수학은 비대면강의입니다.
-https://www.acmicpc.net/problem/19532
 
 색종이
 https://www.acmicpc.net/problem/2563  -> 2차원 배열
