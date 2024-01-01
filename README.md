a1d8e6dd50cb8e63da4c1c2632c7ee33b706f9eb
Jang2723 <jangsb46@gmail.com>
2024-01-01 23:56:27 +0900

    01/01

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
 - [동적 프로그래밍 - Dynamic Programming ](src/W4/ExtraStudy/동적프로그래밍(B2747).md)
 ### [week5](src/W5/README.md)
 - [2차원 가변길이 배열의 크기](src/W5/ArrayLength/arrSize.md)
+### [week6](src/W6/README.md)
---
+++ src/W6/B2745.java
@@ -0,0 +1,41 @@
+package W6;
+
+// https://www.acmicpc.net/problem/2745
+// 진법 변환-일반수학-브2
+/*
+B진법 수 n이 주어진다.
+이를 10진법으로 바꿔 출력하는 프로그램
+10진법으로 넘어가는 진법은 숫자로 표시할 수 없는 자리가 있다
+알파벳 사용 -> A: 10, B: 11, ..., F: 15, ..., Y: 34, Z: 35
+A아스키코드 65 Z 90
+첫 줄에 n과 b가 주어진다.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class B2745 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println((int)('A'-10));
+        String[] str = br.readLine().split(" ");
+        int B = Integer.parseInt(str[1]);
+        int sum= 0;
+        int temp = 1; //몇 승인지
+
+        for (int i = str[0].length()-1; i >= 0; i--) { // 오른쪽부터 계산
+            if ('A' <= (str[0].charAt(i)) && (str[0].charAt(i)) <= 'Z' ){
+                // 10이상, 알파벳
+//                sum += (int)(str[0].charAt(i) - 55) * temp;
+                sum += (str[0].charAt(i) - 'A' + 10) * temp; // A의 아스키 코드 65 - 10진법으로 변환이므로 10더해줌 = 55
+            }
+            else{
+                // 9이하, 숫자일 경우
+                sum += (str[0].charAt(i) -'0') * temp;
+                // -'0'을 해주지 않으면 채점에서 오류가 발생, test case는 맞는데...
+            }
+            temp *= B;
+        }
+        System.out.println(sum);
+    }
+}
