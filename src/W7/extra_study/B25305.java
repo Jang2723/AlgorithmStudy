8be6cc42c0e7362f5d1b0d58d7e66d6d8b49bb49
Jang2723 <jangsb46@gmail.com>
2024-01-16 18:00:07 +0900

    01/16 extra study update

--- src/W7/extra_study/B25305.java
+++ src/W7/extra_study/B25305.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 
 // https://www.acmicpc.net/problem/25305
 // 커트라인 - 브2
@@ -14,6 +15,7 @@ public class B25305 {
     커트라인은 상받은 사람들 중 점수가 가장 낮은 사람
     Arrays.sort에서 Collections.reverseOrder라는 역순 정렬을 사용하기 위해
     scoreInt 배열을 Integer로 선언해주었다.
+    String을 역순 정렬하면 100이 들어올 경우 맨 끝으로 가는 에러가 생김
     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
